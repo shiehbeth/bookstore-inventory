8bda0d79c9953416bbf943f23f484013938ee7c7
Beth <shiehbeth@gmail.com>
2024-07-26 13:17:12 -0700

    simplified main

--- main.c
+++ main.c
@@ -10,20 +10,10 @@ int main() {
     queue* q = create_queue();
 
     // Add some books to the queue
-    book* book1 = (book*)malloc(sizeof(book));
-    strcpy(book1->bookTitle, "Book 1");
-    strcpy(book1->bookAuthor, "Author 1");
-    book1->pubYear = 2001;
-    book1->ISBN = 1234567890;
-    book1->nextBook = book1->prevBook = NULL;
+    book* book1 = createBook("Book 1", "Author 1", 2001, 12345678);
     addBookToQueue(q, book1);
 
-    book* book2 = (book*)malloc(sizeof(book));
-    strcpy(book2->bookTitle, "Book 2");
-    strcpy(book2->bookAuthor, "Author 2");
-    book2->pubYear = 2002;
-    book2->ISBN = 2345678901;
-    book2->nextBook = book2->prevBook = NULL;
+    book* book2 = createBook("Book2", "Author2", 2014, 2385194);
     addBookToQueue(q, book2);
 
     // Remove a book from the queue
